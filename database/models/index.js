54f11af9cb4abaeac770177c105f7ade8e5f397c
Ernandes <eandesjunior@hotmail.com>
2019-09-25 16:03:39 -0300

    Update in database

--- database/models/index.js
+++ database/models/index.js
@@ -5,7 +5,36 @@ const Sequelize = require('sequelize');
 const config = require('../../config/database');
 
 const db = {};
-const sequelize = new Sequelize(config);
+
+if (process.env.NODE_ENV === 'production'){
+    const sequelize = require('sequelize-heroku').connect(require('sequelize'));
+
+    if (sequelize) {
+        sequelize.authenticate().then(function () {
+            var config = sequelize.connectionManager.config;
+            console.log('sequelize-heroku: Connected to ' + config.host + ' as ' + config.username + '.');
+
+            // sequelize.query('SELECT * from firmwares').then(function (res) {
+            //     console.log('1+1=' + res);
+            // });
+
+        }).catch(function (err) {
+            var config = sequelize.connectionManager.config;
+            console.log(config.host)
+            console.log(config.user)
+            console.log(config.password)
+
+
+            console.log('Sequelize: Error connecting ' + config.host + ' as ' + config.user + ': ' + err);
+        });
+    } else {
+        console.log('No environnement variable found.');
+    }
+}else{
+    const sequelize = new Sequelize(config);
+}
+
+
 
 fs.readdirSync(__dirname)
     .filter(file => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
