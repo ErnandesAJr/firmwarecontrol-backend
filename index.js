d104c8e68a6a4f8ae647ec9adc6a39d782849ccc
Ernandes Azevedo Junior <ernandes_azevedo@stash.atlantico.com.br>
2020-06-25 16:14:13 -0300

    Update versions

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const app = require('./src/app');
-const server = require('http').Server(app.server);
+const app = require("./src/app");
+const server = require("http").Server(app.server);
 
-require('dotenv').config();
+require("dotenv").config();
 
 const context = {
-    express: process.env.PORT || 3000,
-    environment: process.env.NODE_ENV || 'development'
+  express: process.env.PORT || 3000,
+  environment: process.env.NODE_ENV || "development",
 };
 
 app.database.initialize(context.environment, { forceDelete: true });
 
 server.listen(context.express, () => {
-    console.log('Backend Firmware Application running at port ' + context.express);
+  console.log(
+    "Backend Firmware Application running at port " + context.express
+  );
 });
